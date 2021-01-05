88e3420ac633eca86ae2d64b9d351734dacc4072
James Bowers <james.t.k.bowers@gmail.com>
2021-01-05 22:00:34 +1300

    Add support for shutter speed block.

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ enum Block {
     Temperature(f32),
     Acceleration(Vec<[i16; 3]>),
     Gyroscope(Vec<[i16; 3]>),
+    ShutterSpeed(Vec<f32>),
 }
 
 fn parse_devc(input: &[u8]) -> IResult<&[u8], Block> {
@@ -273,6 +274,32 @@ fn parse_gyro(input: &[u8]) -> IResult<&[u8], Block> {
     Ok((input, Block::Gyroscope(measurements)))
 }
 
+fn parse_shut(input: &[u8]) -> IResult<&[u8], Block> {
+    let (input, _data_type) = tag(b"f")(input)?;
+    let (input, size) = be_u8(input)?;
+    assert_eq!(size, 4);
+    let (input, count) = be_u16(input)?;
+
+    let mut input = input;
+    let mut measurements = Vec::new();
+    for _ in 0..count {
+        let (iinput, shutter_speed) = be_f32(input)?;
+        measurements.push(shutter_speed);
+        input = iinput; // TODO: tidy up
+    }
+
+    // Take remaining padding bytes
+    let data_length = 6 * measurements.len();
+    let input = if data_length % 4 != 0 {
+        let count_remaining_bytes = 4 - data_length % 4;
+        take(count_remaining_bytes)(input)?.0
+    } else {
+        input
+    };
+
+    Ok((input, Block::ShutterSpeed(measurements)))
+}
+
 fn parse_block(input: &[u8]) -> IResult<&[u8], Block> {
     let (input, block_type) = take(4usize)(input)?;
     let (input, block) = match block_type {
@@ -289,6 +316,7 @@ fn parse_block(input: &[u8]) -> IResult<&[u8], Block> {
         b"TMPC" => parse_tmpc(input),
         b"ACCL" => parse_accl(input),
         b"GYRO" => parse_gyro(input),
+        b"SHUT" => parse_shut(input),
         block_type => {
             println!("Got unexpected block type {:x?} | {:?}", block_type, std::str::from_utf8(block_type).unwrap());
             Err(nom::Err::Failure(nom::error::Error::new(input, ErrorKind::Tag)))
